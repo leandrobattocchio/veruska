fe31abd319c4bcb38a5ca78f0c2a65e2974e3f52
Leandro Battocchio <leandrobattocchio@hotmail.com>
2023-05-03 03:36:00 -0300

    lazy pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Footer from './components/Footer.jsx'
-import Contacto from './pages/Contacto.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.Fragment>
     <App />
-    <Contacto/>
-    <Footer/>
   </React.Fragment>,
 )
