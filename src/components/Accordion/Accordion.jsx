ebfaeffc4c68c16e4fa1d1027e98505f5aa65390
LucasAbbona <lucasabbona0@gmail.com>
2023-05-29 15:39:56 -0300

    Accordion arrow

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react"
-
+import {MdKeyboardArrowDown} from 'react-icons/md'
 const Accordion = ({title,desc}) => {
     const [Show,setShow] = useState(false)
 
     return (
     <div className="w-full px-16 flex flex-col">
         <div className="my-4 border-b border-primary-darkerblue   py-4 flex flex-col gap-4">
-            <p className="md:text-xl text-lg font-semibold cursor-default text-primary-darkerblue" onClick={()=>setShow((sw)=>!sw)}>{title}</p>
+            <div className="flex justify-between items-center" onClick={()=>setShow((sw)=>!sw)}>
+                <p className="md:text-xl text-lg font-semibold cursor-default text-primary-darkerblue" >{title}</p>
+                <MdKeyboardArrowDown size={24}/>
+            </div>
             <p className={Show ? "flex justify-center mb-5 md:text-base text-xs text-primary-black" : "hidden"}>{desc}</p>
         </div>
     </div>
